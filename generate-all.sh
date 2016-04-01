add57770ff8dd1c358f46949c4e919dd01713038
Charles Bailey <cbailey32@bloomberg.net>
2016-04-01 16:05:35 +0100

    Remove generate-all as "generate-larger-than 0" will do this

--- generate-all.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-git cat-file --batch-all-objects --batch-check='%(objectname) %(objectsize:disk) %(objectsize) %(objecttype) %(deltabase)' --buffer |
-    awk '{ if ($4 == "blob") { print $1, $2, $3, $5 }}'
