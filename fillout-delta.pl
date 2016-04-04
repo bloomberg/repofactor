912522ca33e3def66c4b13225cf4fe1e3cfdf09f
Charles Bailey <cbailey32@bloomberg.net>
2016-04-04 18:29:58 -0400

    Add LICENSE and copyright notices

--- fillout-delta.pl
+++ fillout-delta.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/env perl
 
-#
 # Script to print an "average" size-on-disk for delta compressed objects
 # It takes an input format of:
 #   <id> [<other fields>...]
@@ -89,3 +88,17 @@ for (@rows) {
 
 close(batch_check);
 close(batch_result);
+
+#   Copyright (C) 2015,2016 Bloomberg Finance L.P.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
